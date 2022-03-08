e6dad31cd9388039b81dc4a32b8a615f475bf959
JAbozarOid <abozar.raghibdoust@gmail.com>
2022-03-09 03:05:42 +0330

    NFT-01
    set bottom navigation view

--- app/src/main/java/com/arabnetwork/nft/MainActivity.kt
+++ app/src/main/java/com/arabnetwork/nft/MainActivity.kt
@@ -2,10 +2,43 @@ package com.arabnetwork.nft
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.NavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.setupWithNavController
+import com.arabnetwork.nft.R
+import com.arabnetwork.nft.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    /**
+     * binding
+     */
+    private var _binding : ActivityMainBinding? = null
+    private val mBinding get() = _binding
+
+    /**
+     * variables
+     */
+    private lateinit var navController: NavController
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+        _binding = ActivityMainBinding.inflate(layoutInflater)
+
+        setContentView(mBinding?.root)
+
+        val navHostFragment =
+            supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment
+
+        navController = navHostFragment.navController
+
+        // bottomNavigationView is the navigation that access this view from our activity
+        mBinding?.bottomNavView?.setupWithNavController(navController)
+    }
+
+    // for navigating app
+    override fun onSupportNavigateUp(): Boolean {
+        return navController.navigateUp() || super.onSupportNavigateUp()
     }
 }
